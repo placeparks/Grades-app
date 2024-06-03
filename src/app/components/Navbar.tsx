1b4bddd1942543d7eebfc46d8d17d8272d76e574
placeparks <86215469+placeparks@users.noreply.github.com>
2024-06-03 12:29:04 +0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar: React.FC = () => {
     <div>
       <nav className="relative bg-transparent dark:bg-gray-900 fixed w-full z-20 top-0 start-0 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
-          <Link href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image src="/logo1.png" width={124} height={80} className="h-16 w-auto md:h-[80.42px] md:w-[123.81px]" alt="Logo" />
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
